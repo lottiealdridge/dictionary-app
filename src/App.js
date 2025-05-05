59858bb360900d899c5e7dd37cb243473d9d054b
lottiealdridge <lottiealdridge95@gmail.com>
2025-05-05 14:16:58 +0100

    added dictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
       </header>
-      Hello
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="app-footer">
+        Coded by Lottie Aldridge
+      </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+
+    }
+
+    function search(event) {
+        event.preventDefault();
+        alert(`Seaching for ${keyword}`);
+    }
+
+    return(
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange}/>
+            </form>
+        </div>
+    )
+}
