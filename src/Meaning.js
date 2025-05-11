a8abd7cb5e0565178ae883963979fcb9e2585669
lottiealdridge <lottiealdridge95@gmail.com>
2025-05-11 18:12:54 +0100

    rendered the word meanings

---
+++ src/Meaning.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Meaning(props) {
+    console.log(props.meaning)
+    return (
+        <div className="meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>
+            <p>
+                {props.meaning.definition}
+                <br />
+                <em>{props.meaning.example}</em>
+            </p>
+
+        </div>
+    )
+}
